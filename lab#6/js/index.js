1026d6a747e375de2d8b564be5203c19d67e9bca
semenkivv <101628506+semenkivv@users.noreply.github.com>
2022-08-25 21:00:32 +0300

    Add files via upload

---
+++ lab#6/js/index.js
@@ -0,0 +1,27 @@
+const fullpageApi = $.fn.fullpage;
+
+function getHash() {
+    return window.location.hash;
+}
+function setHash(value) {
+    window.location.hash = value;
+}
+$('.fullpage').fullpage({
+	anchors:['Globe_Arena', 'StatueOfABoy', 'Royal_Palace']
+});
+$('.fullpage').fullpage({
+    anchors:['firstPage', 'secondPage', 'thirdPage'],
+    lockAnchors: true,
+    onLeave: function (origin, destination, direction, trigger) {
+        $('.section[data-anchor="' + origin.anchor + '"]').removeClass('vis');
+        let destinationAnchor = $('[data-index="' + destination.index + '"]').attr('data-anchor');
+        setHash(destinationAnchor);
+    },
+    afterLoad: function (origin, destination, direction, trigger) {
+        $('.section[data-anchor="' + destination.anchor + '"]').addClass('vis');
+    }
+});
+
+$('[data-section]').on('click', function () {
+    fullpageApi.moveTo($(this).attr('data-section'));
+});
